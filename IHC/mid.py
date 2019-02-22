da47d240be50106c4b9545e015a28126df1c3bc1
Guilherme <guilherme.anderson34@outlook.com>
2019-02-22 11:43:17 -0300

    Desenvolvendo LolBot, ja funcional, msa com pendências a serem resolvidas

---
+++ IHC/mid.py
@@ -0,0 +1,9 @@
+def coco():
+    coco = [
+        '| coco, a bunda de Noxus | Dano Físico | Lutador | Top |',
+        'Fraco contra: Teemo, Jayce, Yorick, Kayle, Renekton, Aatrox',
+        'Forte contra: Yasuo, Nasus, Wucong, Garen, Irelia, Riven',
+        'Itens Populares: Cutelo Negro, Semblante Espiritual, Couraça do Defundo, Bota de Resistência, Mágica ou '
+        'armadura, Anjo Guardião'
+    ]
+    return coco
---
+++ IHC/top.py
@@ -0,0 +1,108 @@
+def darius():
+    Darius = [
+        '| Darius, a mão de Noxus | Dano Físico | Lutador | Top |',
+        'Fraco contra: Teemo, Jayce, Yorick, Kayle, Renekton, Aatrox',
+        'Forte contra: Yasuo, Nasus, Wucong, Garen, Irelia, Riven',
+        'Itens Populares: Cutelo Negro, Semblante Espiritual, Couraça do Defundo, Bota de Resistência, Mágica ou '
+        'armadura, Anjo Guardião'
+    ]
+    return Darius
+
+
+def pantheon():
+    pantheon = [
+            '| O artesão da Guerra | Dano Físico | Lutador | Top | Jungle',
+            'Fraco contra: Shen, Malphite, Yorick, Udyr, Darius, Elise',
+            'Forte contra: Fiora, Riven, Gangplank, Garen, Teemo, Jax',
+            'Itens Populares: Cutelo Negro, Lamina Fantasma de Yumu, Crepusculo de Draktharr, Bota de Resistência '
+            'Mágica ou armadura, Anjo Guardião'
+    ]
+    return pantheon
+
+
+def riven():
+    riven = [
+            '| A Exilada | Dano Físico | Lutador | Top |',
+            'Fraco contra: Garen, Renekton, Pantheon, Olaf, Jax, Kennen',
+            'Forte contra: Yasuo, Gnar, Nasus, Diana, Vladmir, Aatrox',
+            'Itens Populares: Cutelo Negro, Lamina Fantasma de Yumu, Hidra Raivosa, Bota de Resistência Mágica ou '
+            'armadura, Dansa da Morte, Anjo Guardião'
+    ]
+    return riven
+
+
+def garen():
+    garen = [
+            '| O poder de Demacia | Dano Físico | Lutador | Top | Tank |',
+            'Fraco contra: Teemo, Pantheon, Darius, Jayce, Yorick, Gnar',
+            'Forte contra: Riven, Renekton, Jax, Nasus, Akali, Wucong',
+            'Itens Populares: Cutelo Negro, Semblante Espiritual, Couraça do Defundo, Bota de Resistência, Mágica ou '
+            'armadura, Fúria de Sterak, Anjo Guardião'
+    ]
+    return garen
+
+
+def aatrox():
+    aatrox = [
+            '| A Espada Darkin | Dano Físico | Lutador | Top | Jungle |',
+            'Fraco contra: Fiora, Jax, Riven, Tryndamere, Ekko, Renekton',
+            'Forte contra: Darius, Master Yi, Zed, Yasuo, Katarina, Hecarim',
+            'Itens Populares: Cutelo Negro, Dansa da Morte, Fúria de Sterak, Couraça do Defunto, Bota de armadura ou '
+            'resistência Mágica, Anjo Guardião '
+    ]
+    return aatrox
+
+
+def malphite():
+    malphite = [
+            '| O Fragmento do Monolito | Top | Dano Mágico | Tank | Top | Jungle |',
+            'Fraco contra: Rumble, Vladmir, Shen, Yorick, Zac',
+            'Forte contra: Jax, Camille, Fiora, Tryndamere, Pantheon, Xin Zhao',
+            'Itens Populares: Capa de Fogo Solar, Manopla dos Glacinatas, Elmo Adaptativo, Armadura de espinhos, '
+            'Coração Congelado, Bota de armadura ou resistência Mágica '
+    ]
+    return malphite
+
+
+def wucong():
+    wucong = [
+            '| o Macaco Rei | Dano Físico | Lutador | Top |',
+            'Fraco contra: Darius, Garen, Lee Sin, Olaf',
+            'Forte contra: Zed, Jayce, Jayce, Teemo, Master Yi, Aatrox',
+            'Itens Populares: Cutelo Negro, Semblante Espiritual, Couraça do Defundo, Bota de Resistência, Mágica ou '
+            'armadura, Anjo Guardião'
+    ]
+    return wucong
+
+
+def sion():
+    sion = [
+            '| O Macaco Rei | Dano Físico | Lutador | Tank | Top | Jungle',
+            'Fraco contra: Darius, Aurelion Sol, Garen, Morgana, Camille, Jax',
+            'Forte contra: LeBlanc, Tryndamere, Talon, Kassadin, Aatrox, Lux',
+            'Itens Populares: Capa de Fogo Solar, Glória Íntegra, Placa Gargolítica,, Elmo Adaptativo, Botas de '
+            'Armadura ou Resistência Mágica, Armadura de Espinhos'
+    ]
+    return sion
+
+
+def maokai():
+    maokai = [
+            '| O Ente Retorcido | Dano Mágico | Tank | Top | Jungle |',
+            'Fraco contra: Shyvana, Dr. Mundo, Nasus, Yorick, Shaco, Swain',
+            'Forte contra: Riven, Karthus, Elise, Kassadin, Katarina, Volibear',
+            'Itens Populares: Capa de Fogo Solar, Armadura de Espinhos, Semblante Espiritual, Presságio de Randuin, '
+            'Armadura de espinhos, Botas de Armadura ou Resistência Mágica'
+    ]
+    return maokai
+
+
+def nasus():
+    nasus = [
+            '| O Curador das Areias | Dano Físico | Tank | Top | Lutador |',
+            'Fraco contra: Teemo, Darius, Garen, Rumble, Pantheon, Riven',
+            'Forte contra: Irelia, Hecarim, Wucong, Aatrox, Tryndamere, Maokai',
+            'Itens Populares: Força da Trindade, Semblante Espiritual, Coração Congelado, Fúria de Sterak, Botas de '
+            'Armadura ou Resistência Mágica, Couraça do Defunto'
+    ]
+    return nasus
